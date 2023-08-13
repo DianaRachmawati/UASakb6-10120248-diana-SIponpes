971ae2c54da2fe00e1cefbe3330a71882d3530d8
unknown <dianarachmawati272@gmail.com>
2023-08-14 02:12:53 +0700

    Initial commit

---
+++ app/src/main/java/com/diana/daftarpesantren/provinsi/ModelMain.java
@@ -0,0 +1,27 @@
+package com.diana.daftarpesantren.provinsi;
+
+import java.io.Serializable;
+
+
+
+public class ModelMain implements Serializable {
+
+    String id;
+    String nama;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+}
---
+++ app/src/main/java/com/diana/daftarpesantren/utils/Constant.java
@@ -0,0 +1,11 @@
+package com.diana.daftarpesantren.utils;
+
+
+public class Constant {
+
+    public static String provinsiId;
+    public static String provinsiName;
+    public static String kabupatenId;
+    public static String kabupatenName;
+
+}
